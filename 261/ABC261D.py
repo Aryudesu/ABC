f3f60b6a3ed8dfea585bc6b0b2e01eda9d9f6923
Aryudesu <suwako_a-u-@hotmail.co.jp>
2022-07-24 20:29:51 +0900

    261Dのコード整理

--- 261/ABC261D.py
+++ 261/ABC261D.py
@@ -1,24 +1,22 @@
 N, M = [int(l) for l in input().split()]
-X = [int(l) for l in input().split()]
-X = [0] + X
+X = [0] + [int(l) for l in input().split()]
 CY = [0 for l in range(N + 1)]
 
+# ボーナスを整理
 for m in range(M):
     C, Y = [int(l) for l in input().split()]
     CY[C] += Y
 
-# print(X)
-# print(CY)
 # 開始前は0点
 res = [[0 for l in range(N + 1)]]
-ZERO = 0
 for n in range(1, N + 1):
     # 1回前の結果
     bdat = res[n - 1]
     # 今回の結果
     tmp = [0 for l in range(N + 1)]
-    # 各カウントに対しての最大値
+    # 初期に戻る際に最大値を選ぶ
     m = 0
+    # 各カウントに対しての最大値
     for idx in range(0, n):
         # 今回の結果 = 前回の結果 + 今回の得点 + 今回のカウントボーナス
         nx = bdat[idx] + X[n] + CY[idx + 1]
@@ -29,8 +27,8 @@ for n in range(1, N + 1):
             tmp[idx + 1] = nx
         else:
             tmp[idx + 1] = bdat[idx + 1]
+    # カウント0の時最大値を選びたい
     if bdat[0] < m:
         tmp[0] = m
     res.append(tmp)
-    # print(res)
 print(max(res[-1]))
