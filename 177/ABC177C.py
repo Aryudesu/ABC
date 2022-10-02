9e12e5d736bcf04e7fc4e39c0b144af6ba52d5da
Aryudesu <suwako_a-u-@hotmail.co.jp>
2022-10-02 20:41:53 +0900

    フォーマッタ

--- 177/ABC177C.py
+++ 177/ABC177C.py
@@ -1,14 +1,14 @@
-MOD= 10**9 + 7
+MOD = 10**9 + 7
 N = int(input())
 A = [int(l) % MOD for l in input().split()]
 S = [0]*N
 s = 0
 
 for i in range(1, N+1):
-    s = (s + A[-i])%MOD
+    s = (s + A[-i]) % MOD
     S[-i] = s
 
 res = 0
 for i in range(N-1):
-    res = (res + A[i]*S[i+1])%MOD
+    res = (res + A[i]*S[i+1]) % MOD
 print(res)
