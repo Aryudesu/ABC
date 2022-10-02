3766bc2b8012b63da3b625c6c4e9bf66366b92dd
Aryudesu <suwako_a-u-@hotmail.co.jp>
2022-10-02 17:04:36 +0900

    B修正

--- 271/ABC271B.py
+++ 271/ABC271B.py
@@ -1,13 +1,8 @@
 N, Q = [int(l) for l in input().split()]
 A = []
-L = []
 for n in range(N):
-    l, *a = [int(l) for l in input().split()]
-    A.append(a)
-    L.append(l)
+    A.append([int(l) for l in input().split()])
 res = []
 for q in range(Q):
     s, t = [int(l) for l in input().split()]
-    res.append(A[s-1][t-1])
-for r in res:
-    print(r)
+    print(A[s-1][t])
