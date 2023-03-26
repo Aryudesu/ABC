0024caf0a74ed7e0f54bca23098fba10d34f4546
Aryudesu <suwako_a-u-@hotmail.co.jp>
2023-03-26 22:28:08 +0900

    288C

--- 288/ABC288C.py
+++ 288/ABC288C.py
@@ -1,39 +1,85 @@
-COUNT = 0
+from collections import defaultdict
 
 
-def calc(now, N, M, Node, graph):
-    # print(now)
-    global COUNT
-    ALL[now] = True
-    path = graph.get(now, [])
-    Node[now] = True
-    lp = len(path)
-    # 行き止まり
-    f = False
-    for p in path:
-        # 進める
-        if not Node[p]:
-            Node[p] = True
-            f = True
-            calc(p, N, M, Node, graph)
-            Node[p] = False
-    if not f:
-        print(now)
-        COUNT += 1
-    Node[now] = False
+class UnionFind():
+    """Union Find木クラス"""
+
+    def __init__(self, n):
+        """初期化"""
+        self.n = n
+        self.root = [-1]*(n+1)
+        self.rank = [0]*(n+1)
+
+    def find(self, x):
+        """ノードxの根を見つける"""
+        if self.root[x] < 0:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+
+    def unite_root(self, x, y):
+        """グループの統合を行いグループの統合先と統合元を返却する"""
+        x = self.find(x)
+        y = self.find(y)
+        if x == y:
+            return x, x
+        elif self.rank[x] > self.rank[y]:
+            self.root[x] += self.root[y]
+            self.root[y] = x
+            return x, y
+        else:
+            self.root[y] += self.root[x]
+            self.root[x] = y
+            if self.rank[x] == self.rank[y]:
+                self.rank[y] += 1
+            return y, x
+
+    def unite(self, x, y):
+        """木の統合"""
+        self.unite_root(x, y)
+
+    def multi_unite(self, xy):
+        """複数統合"""
+        for dat in xy:
+            self.unite(dat[0], dat[1])
+
+    def unite_same(self, x, y):
+        """統合前に同じグループに属するかの判定"""
+        a, b = self.unite_root(x, y)
+        return a == b
+
+    def same(self, x, y):
+        """同じグループに属するか判定"""
+        return self.find(x) == self.find(y)
+
+    def size(self, x):
+        """木のサイズを計算"""
+        return -self.root[self.find(x)]
+
+    def roots(self):
+        """根のノードを取得"""
+        return [i for i, x in enumerate(self.root) if x < 0]
+
+    def max_tree_size(self):
+        """最大の木のサイズを取得"""
+        return max([self.size(r) for r in self.roots()])
+
+    def group_size(self):
+        """グループ数を取得"""
+        return len(self.roots())
+
+    def group_members(self):
+        """全てのグループごとのノードを取得"""
+        group_members = defaultdict(list)
+        for member in range(self.n):
+            group_members[self.find(member)].append(member)
+        return group_members
 
 
 N, M = [int(l) for l in input().split()]
-ALL = [False] * N
-graph = dict()
-Node = [False] * N
+uf = UnionFind(N)
 for m in range(M):
-    A, B = [int(l) for l in input().split()]
-    tmp = graph.get(A - 1, [])
-    tmp.append(B - 1)
-    graph[A - 1] = tmp
-    tmp = graph.get(B - 1, [])
-    tmp.append(A-1)
-    graph[B-1] = tmp
-calc(0, N, M, Node, graph)
-print(COUNT)
+    a, b = [int(l) for l in input().split()]
+    uf.unite(a, b)
+gs = uf.group_size() - 1
+print(M - (N - gs))
