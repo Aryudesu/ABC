d9130f993047ee87d7ae523a7c8f778035f4637e
Aryudesu <suwako_a-u-@hotmail.co.jp>
2023-07-16 19:03:07 +0900

    310D

--- 310/D.py
+++ 310/D.py
@@ -1,21 +1,49 @@
 N, T, M = [int(l) for l in input().split()]
-graph = dict()
+person = dict()
 Team = []
 for t in range(T):
     tmp = set()
     Team.append(tmp)
-bad = set()
 for m in range(M):
     a, b = [int(l) for l in input().split()]
-    bad.add(a)
-    bad.add(b)
-    tmp = graph.get(a, [])
-    tmp.append(b)
-    graph[a] = tmp
-    tmp = graph.get(b, [])
-    tmp.append(a)
-    graph[b] = tmp
+    tmp = person.get(a)
+    if tmp is None:
+        tmp = set()
+    tmp.add(b)
+    person[a] = tmp
+    tmp = person.get(b)
+    if tmp is None:
+        tmp = set()
+    tmp.add(a)
+    person[b] = tmp
+
+result = 0
 
 
-def partition(AB, node):
-    pass
+def partition(num, N, TN):
+    if N - num < T - TN:
+        return
+    if num == N:
+        for t in Team:
+            if not t:
+                return
+        global result
+        result = result + 1
+        return
+    for t in range(T):
+        tmp = Team[t]
+        tmp2 = person.get(num + 1)
+        # 相性悪い組み合わせが存在すると計算しない
+        if tmp2 is not None:
+            if len(tmp & tmp2):
+                continue
+        # チームに追加
+        Team[t].add(num+1)
+        partition(num+1, N, TN if t < TN else TN + 1)
+        Team[t].remove(num+1)
+        if not Team[t]:
+            break
+
+
+partition(0, N, 0)
+print(result)
