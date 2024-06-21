69a508afa8a1b119d87eacf13f8860129d571117
Aryudesu <suwako_a-u-@hotmail.co.jp>
2024-06-22 00:30:00 +0900

    301D

--- ABC/301/ABC301D.py
+++ ABC/301/ABC301D.py
@@ -1,33 +1,22 @@
-def calcBit(N):
-    result = []
-    if N == 0:
-        return [0]
-    tmpN = N
-    while tmpN:
-        result.append(tmpN % 2)
-        tmpN >>= 1
-    return result
-
-
-def calc(S, N):
-    bitN = calcBit(N)
-    if len(bitN) > len(S):
-        while len(bitN)>len(S):
-            S.append("0")
-    elif len(bitN) < len(S):
-        while len(bitN) < len(S):
-            S.append(0)
-    bitN.reverse()
-    S.reverse()
-    print(bitN)
-    print(S)
-    lenBN = len(bitN)
-    sameF = True
-    result = 0
-    for _ in range(lenBN):
-        pass
-
-
 S = list(input())
+S.reverse()
 N = int(input())
-print(calc(S, N))
+num = 0
+data = []
+t = 1
+for idx in range(len(S)):
+    s = S[idx]
+    tmp = 1 << idx
+    if s == "?":
+        data.append(tmp)
+    elif s == "1":
+        num |= tmp
+data.sort(reverse=True)
+if num > N:
+    print(-1)
+else:
+    result = num
+    for dat in data:
+        if (result | dat) <= N:
+            result |= dat
+    print(result)
