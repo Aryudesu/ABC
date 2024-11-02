c9dbfaf5032cdadf4ad431dabeb41101550d1eee
Aryudesu <suwako_a-u-@hotmail.co.jp>
2024-11-02 20:54:50 +0900

    ファイル作成

--- fourSq.py
+++ fourSq.py
@@ -1,24 +1,19 @@
 # 4平方和定理
-
-def input_data(N: int)-> list:
-    return [int(input()) for _ in range(N)]
-
 def calc(data: list)-> int:
+    if not data:
+        return 1
     MOD = 998244353
     data_dict = dict()
-
     for n in data:
         if n == 2 and n in data_dict:
             continue
-        data_dict[n] = (data_dict.get(n, 1) * n + 1) % MOD
-
-    result = 1
-
+        data_dict[n] = (data_dict.get(n, 1) * n + 1)%MOD
+    result = 8
     for k in data_dict:
-        result *= data_dict.get(k, 1)
+        result = (result * data_dict.get(k, 1)) % MOD
+    return result
 
-    return result * 8
-
-nums = input_data(int(input()))
+_ = int(input())
+nums = [int(l) for l in input().split()]
 result1 = calc(nums)
 print(result1)
