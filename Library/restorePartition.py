d1714d1dc6739d116aada5fc9e13ed559e735be9
Aryu <suwako_a-u-@hotmail.co.jp>
2025-10-25 19:40:48 +0900

    ライブラリ整え

--- Library/restorePartition.py
+++ Library/restorePartition.py
@@ -1,15 +1,17 @@
 class RestorePartition:
-    def __init__(self, N: int, A: list[int]):
+    def __init__(self, N: int, A: list[int], maxSum: int = -1):
         """
         復元可能部分和問題．
         長さNの数列Aのうち解がSになる組み合わせが存在するか確認し，
         するならば復元できるようにする．
+        最大maxSum以降は考慮しないようにするが，maxSum <= 0の場合は全組み合わせを考慮する．
         """
         self.N = N
         self.A = A
+        self.maxSum = maxSum
         self.data = self._calcPartition()
 
-    def _calcPartition(self) -> dict[int, int|None]|None:
+    def _calcPartition(self) -> dict[int, int|None]:
         """
         事前計算を行います．
         """
@@ -18,8 +20,10 @@ class RestorePartition:
         for idx, a in enumerate(self.A):
             new_data = parent.copy()
             for k in parent:
-                next = k + a
-                if not next in new_data:
+                nxt = k + a
+                if self.maxSum > 0 and self.maxSum < nxt:
+                    continue
+                if not nxt in new_data:
                     new_data[k + a] = idx
             parent = new_data
         return parent
@@ -32,7 +36,7 @@ class RestorePartition:
 
     def makeData(self, S: int) -> list[int]:
         """
-        Sになる組み合わせのIndex配列を返却します．
+        Sになる組み合わせの0-basedであるIndex配列を返却します．
         存在しない場合は空配列を返却します．
         """
         result = []
@@ -41,13 +45,21 @@ class RestorePartition:
         tmpS = S
         while tmpS > 0:
             prevIdx = self.data[tmpS]
+            if prevIdx is None:
+                raise RuntimeError("再構築時エラー（逆伝搬時Noneエラー）")
             result.append(prevIdx)
             tmpS -= self.A[prevIdx]
-        if tmpS < 0:
-            raise Exception()
+        if tmpS != 0:
+            raise RuntimeError("再構築時エラー（逆伝搬の結果0に戻らなかった）")
         result.reverse()
         return result
+    
+    def makeValues(self, S) -> list[int]:
+        """合計がSになる組み合わせを1つ返却します．"""
+        dat = self.makeData(S)
+        return [self.A[idx] for idx in dat]
 
+# == 競技プログラミングの鉄則B18
 N, S = map(int, input().split())
 A = [int(l) for l in input().split()]
 rp = RestorePartition(N, A)
