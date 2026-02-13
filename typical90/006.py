a93317b0a4974dccd71dfb11f2c2ce16606bab34
Aryu <suwako_a-u-@hotmail.co.jp>
2026-02-14 00:04:08 +0900

    テンプレ化

--- typical90/006.py
+++ typical90/006.py
@@ -1,7 +1,25 @@
+from heapq import heappop, heappush
+
+def smallestSubset(S: str, K: int):
+    """Sの部分列で辞書順最小の長さKのものを取得"""
+    N = len(S)
+    data = []
+    prev = -1
+    result = []
+    for i in range(N):
+        heappush(data, (S[i], i))
+        if i < N - K:
+            continue
+        while True:
+            c, j = heappop(data)
+            if prev > j:
+                continue
+            prev = j
+            result.append(c)
+            break
+    return "".join(result)
+
+# === 競プロ典型006
 N, K = [int(l) for l in input().split()]
 S = input()
-data = []
-for i in range(N):
-    data.append((S[i], i))
-data.sort()
-print(data)
+print(smallestSubset(S, K))
